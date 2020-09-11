1902c4196054924728640bf484c42ec4f2790f42
Rohan Battula <rohanbattula@gmail.com>
2020-09-11 14:15:25 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import praw
+import requests
+import discord
+from discord import Webhook, RequestsWebhookAdapter, File
+
+# ENTER YOUR WEBHOOK ID AND WEBHOOK TOKEN HERE
+id = ""
+token = ""
+
+# ENTER REDDIT CLIENT ID, SECRET, USER AGENT, USERNAME PASSWORD
+# User agent example : Python:com.rohan.fashionscraper:v1
+reddit = praw.Reddit(client_id="",
+                     client_secret="",
+                     user_agent="",
+                     username="",
+                     password="")
+
+WEBHOOK_ID = id
+WEBHOOK_TOKEN = token
+# Create webhook
+webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN,\
+ adapter=RequestsWebhookAdapter())
+
+#submission_stream = (reddit.freps.stream.submissions(pause_after=-1)
+
+for submission in reddit.subreddit("fashionrepsbst").stream.submissions():
+        print(submission.title)
+        if submission is None:
+                continue
+        if("USA" in submission.title):
+            embd = discord.Embed()
+            embd.description = submission.title
+            embd.title= submission.url
+            webhook.send(embed=embd)
